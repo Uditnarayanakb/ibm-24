c3aa3509cc6e2ad6c394d596f93431863e2ad6d2
Uditnarayanakb <uditnarayanakb@gmail.com>
2024-10-20 11:44:48 +0530

    updated

--- auth/src/appwriteConfig.jsx
+++ auth/src/appwriteConfig.jsx
@@ -2,7 +2,7 @@
 import { Client, Account } from 'appwrite';
 
 export const API_ENDPOINT = 'https://cloud.appwrite.io/v1'
-export const PROJECT_ID = '670f48b40035eeadb5ec'
+export const PROJECT_ID = 'ur api id'
 
 const client = new Client()
     .setEndpoint(API_ENDPOINT) 
